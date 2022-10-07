36f47d4e3bacfb9e3434c51b7c0566f27cca12cc
Leon Wolf <leon.wolf@check24.de>
2022-10-07 23:53:42 +0200

    Initial Commit

---
+++ betcha/betchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  betchaApp.swift
+//  betcha
+//
+//  Created by Leon Wolf on 07.10.22.
+//
+
+import SwiftUI
+
+@main
+struct betchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
