4eea079c3e238bc4dea2ad61933dcbce508410fd
Leon Wolf <leon@leonwolf.dev>
2022-10-20 23:32:35 +0200

    basic detail view

--- betcha/Views/Home/HomeView.swift
+++ betcha/Views/Home/HomeView.swift
@@ -12,36 +12,31 @@ struct HomeView: View {
     @EnvironmentObject private var entryVM: EntryViewModel
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
+        NavigationView {
+            List {
                 ForEach(entryVM.entryList, id: \.id) { entry in
-                    HStack {
-                        Text(entry.label)
-                        Text(entry.createdAt)
-                    }
-                    .background {
-                        Color.red
+                    NavigationLink(
+                        destination: DetailView(entry: entry)
+                    ) {
+                        VStack(alignment: .leading) {
+                            Text(entry.label)
+                            Text(entry.createdAt)
+                        }
                     }
                 }
             }
-        }
-        .refreshable {
-            Task {
-                await entryVM.getEntries()
-            }
-        }
-        .onAppear {
-            if entryVM.entryList.count == 0 {
+            .refreshable {
                 Task {
                     await entryVM.getEntries()
                 }
             }
+            .onAppear {
+                if entryVM.entryList.count == 0 {
+                    Task {
+                        await entryVM.getEntries()
+                    }
+                }
+            }
         }
-    }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
     }
 }
